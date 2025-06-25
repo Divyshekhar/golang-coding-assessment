e1f435e7bb2a1d195b74e95d9b876dd9e5bda19c
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-06-25 20:57:58 +0530

    add: user_controller tests

---
+++ tests/user_controller_test.go
@@ -0,0 +1,57 @@
+package tests
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Divyshekhar/golang-coding-assessment/controllers"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSignup(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	router.POST("/user/create", controllers.Signup)
+	body := map[string]string{
+		"name":     "Test User",
+		"email":    "test@patient.com",
+		"password": "testpassword",
+		"role":     "doctor",
+	}
+	jsonBody, _ := json.Marshal(body)
+
+	req, err := http.NewRequest(http.MethodPost, "/user/signup", bytes.NewBuffer(jsonBody))
+	req.Header.Set("Content-Type", "application/json")
+	assert.NoError(t, err)
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+	assert.Equal(t, 200, resp.Code)
+
+}
+func TestLogin(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	router.POST("/user/login", controllers.Login)
+	body := map[string]string{
+		"email":    "test@patient.com",
+		"password": "testpassword",
+	}
+	jsonBody, err := json.Marshal(body)
+	assert.NoError(t, err)
+
+	req, err := http.NewRequest(http.MethodPost, "/user/login", bytes.NewBuffer(jsonBody))
+	assert.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+
+	assert.Equal(t, http.StatusOK, resp.Code)
+
+	t.Log("Response Body:", resp.Body.String())
+
+}

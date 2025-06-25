4808db114606db07563cbd01b51b4b68d13f8190
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-06-25 12:51:43 +0530

    fix: check exp

--- middleware/auth.go
+++ middleware/auth.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
@@ -13,23 +14,28 @@ func RequireAuth() gin.HandlerFunc {
 		tokenStr, err := ctx.Cookie("jwt_token")
 		if err != nil {
 			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "unautheticated"})
-
+			return
 		}
 		token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
 			return []byte(os.Getenv("JWT_SECRET")), nil
 		})
 		if err != nil {
 			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+			return
 		}
 
 		claims, ok := token.Claims.(jwt.MapClaims)
 		if !ok || !token.Valid {
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token or expired"})
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+			return
+		}
+		if exp, ok := claims["exp"].(float64); !ok || float64(time.Now().Unix()) > exp {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "token expired"})
 			return
 		}
 		userId := claims["user_id"]
 		role := claims["role"]
-		ctx.Set("userId", userId)
+		ctx.Set("user_id", userId)
 		ctx.Set("role", role)
 		ctx.Next()
 
